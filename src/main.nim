0f1b5e800db8546c4bd62792c451c6daf29a8526
FelixMo42 <felix@tacocat.com>
2020-07-21 22:10:57 -0700

    new scopint system and more errors

--- src/main.nim
+++ src/main.nim
@@ -11,15 +11,16 @@ type
     Var = object
         kind : Type
 
-    Scope = object
+    Scope = ref object
         vars : TableRef[string, Var]
+        prev : Scope
 
 # some usefull logging functions
 
 const
     Err = "\e[31mERROR\e[0m "
 
-proc tab(txt: string) : string = txt.indent(1, "\t")
+proc tab(txt: string) : string = txt.indent(1, "   ")
 
 converter toString*(a: Type): string =
     if a.args.len == 0 :
@@ -30,15 +31,27 @@ converter toString*(a: Type): string =
 converter toString*(a: Var): string = a.kind.base
 
 converter toString*(a: ValueNode): string =
-    var txt = ""
+    var txt = "~" & $a.kind
 
-    txt &= $a.kind
     if a.kind == CallFunc :
-        txt &= "\n" & a.fn.toString().tab()
+        txt &= "\n" & (a.fn.toString()).tab()
+
+        txt &= "\n" & " args: "
+        for arg in a.args :
+            txt &= "\n" & arg.toString().tab()
+
     if a.kind == Variable :
         txt &= ": " & a.name
+    if a.kind == MakeFunc :
+        txt &= " : "
+        for param in a.params :
+            txt &= param.name & "(" & param.kind.toString() & ")" & " "
+        txt &= "-> " & a.ret.toString()
+        txt &= "\n"  & a.value.toString().tab()
 
     return txt
+
+#
 
 proc fits(a: Type, b: Type): bool =
     # make sure they have the same base type
@@ -54,13 +67,36 @@ proc fits(a: Type, b: Type): bool =
 
     return true
 
+proc get(scope: Scope, name: string): Var =
+    if scope.vars.hasKey(name) :
+        return scope.vars[name]
+
+    if scope.prev != nil :
+        return scope.prev.get(name)
+
+    echo Err, "undefined variable"
+
+    return Var(kind: Type(base: "fault"))
+
 proc getVar(value: ValueNode, scope: Scope): Var =
     case value.kind :
     of MakeFunc :
         var kind = Type(base: "fn", args: @[])
 
+        let funcScope = Scope(
+            vars: newTable[string, Var](),
+            prev: scope
+        )
+
         for param in value.params :
+            # get the type of the paramater
+            let paramKind = getVar(param.kind, scope).kind
+
+            # add it to the type specification of the functions
             kind.args.add( getVar(param.kind, scope).kind )
+
+            # add the paramater to the scope
+            funcScope.vars[param.name] = Var(kind: paramKind)
 
         # get the return type of the function
         let ret = getVar(value.ret, scope).kind
@@ -69,7 +105,7 @@ proc getVar(value: ValueNode, scope: Scope): Var =
         kind.args.add( ret )
 
         # get the type that is being returned by the function
-        let value = getVar(value.value, scope)
+        let value = getVar(value.value, funcScope)
 
         # if the declared return type and real return type dont match, error
         if not value.kind.fits(ret) :
@@ -77,11 +113,23 @@ proc getVar(value: ValueNode, scope: Scope): Var =
 
         return Var(kind: kind)
     of CallFunc :
-        return Var(kind: getVar(value.fn, scope).kind.args[^1])
+        # get the function were calling
+        let fn = getVar(value.fn, scope)
+
+        # get the expected paramaters of the functions
+        let params = fn.kind.args[0..^2]
+
+        # check if they have the same number of arguments
+        if value.args.len != params.len :
+            echo Err, "unexpected number of arguments"
+
+        for i in 0..<min(value.args.len, params.len):
+            if not getVar(value.args[i], scope).kind.fits( params[i] ) :
+                echo Err, "wrong paramater tye"
+
+        return Var(kind: fn.kind.args[^1])
     of Variable :
-        if scope.vars.hasKey(value.name) :
-            return scope.vars[value.name]
-        return Var(kind: Type(base: "fault"))
+        return scope.get(value.name)
     of StrValue :
         return Var(kind: Type(base: "i32"))
     of IntValue :
@@ -97,7 +145,9 @@ let f64* = Type(base: "f64")
 
 baseScope.add("int", Var(kind: i32))
 baseScope.add("+", Var(kind: Type(base: "func", args: @[i32, i32, i32])))
-baseScope.add("1", Var(kind: f64))
+baseScope.add("1", Var(kind: i32))
+
+# echo ast
 
 # echo getVar(ast, Scope(vars: baseScope))
 discard getVar(ast, Scope(vars: baseScope))
